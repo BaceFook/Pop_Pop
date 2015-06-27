5d142fb233a4a534c815891e2e577c17471d8991
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-27 07:54:33 +0300

    Simple game initiator

---
+++ Assets/Scripts/gameInitiator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameInitiator : MonoBehaviour {
+
+	public int bubblesX = 16;
+	public int bubblesY = 16;
+
+	public Transform normalBubble;
+
+	void Start () {
+		float startX = -2.0f * bubblesX / 2.0f;
+		float startY = -2.0f * bubblesY / 2.0f;
+
+		for (int i = 0; i < bubblesX; i++) {
+			for (int j = 0; j < bubblesY; j++) {
+				Instantiate (normalBubble, new Vector3(startX + 2.0f * i, startY + 2.0f * j, 0), Quaternion.identity);
+			}
+			
+		}
+	}
+
+}
