ed88925a74c0ff6683d86fc07c680723ca7d7bb4
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-27 15:31:02 +0300

    Buttons work now

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuController : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log (name);
+		Application.LoadLevel (name);
+	}
+}
