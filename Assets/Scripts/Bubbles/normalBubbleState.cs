938e3fd09ce03b76ca0ce28cb7b251638491fb1f
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-27 07:23:52 +0300

    Refactor of bubble scripts and prefabs

---
+++ Assets/Scripts/Bubbles/normalBubbleState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class normalBubbleState : MonoBehaviour {
+	public bool isPopped = false;
+
+	public Sprite normalSprite;
+	public Sprite poppedSprite;
+
+	void OnMouseDown(){
+		if (isPopped) {
+			Handheld.Vibrate ();
+			return;
+		}
+
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.sprite = poppedSprite;
+		isPopped = true;
+	}
+}
