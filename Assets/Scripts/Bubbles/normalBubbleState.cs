fee545068ab9bc82f6c588c86805e4d243b9a30b
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-27 18:00:08 +0300

    Basic gameplay done

--- Assets/Scripts/Bubbles/normalBubbleState.cs
+++ Assets/Scripts/Bubbles/normalBubbleState.cs
@@ -3,18 +3,53 @@ using System.Collections;
 
 public class normalBubbleState : MonoBehaviour {
 	public bool isPopped = false;
+	public bool isLocked = false;
+	public bool isKey = false;
 
 	public Sprite normalSprite;
 	public Sprite poppedSprite;
 
 	void OnMouseDown(){
-		if (isPopped) {
+		if (isPopped || isLocked) {
 			//Handheld.Vibrate ();
 			return;
 		}
 
+		if (isKey) {
+			GameObject.Find ("GameController").BroadcastMessage("unlockNextBubble");
+			isKey = false;
+		}
+		popBubble ();
+	}
+
+	void popBubble(){
 		SpriteRenderer renderer = transform.FindChild ("bubble").GetComponent<SpriteRenderer>();
 		renderer.sprite = poppedSprite;
 		isPopped = true;
 	}
+
+	void startPopped(){
+		popBubble ();
+	}
+
+	void lockBubble(){
+		SpriteRenderer renderer = transform.FindChild ("lock").GetComponent<SpriteRenderer>();
+		renderer.enabled = true;
+		isLocked = true;
+	}
+	
+	void startLocked(){
+		lockBubble ();
+	}
+
+	void startWithKey(){
+		isKey = true;
+	}
+	
+	void unlockBubble(){
+		SpriteRenderer renderer = transform.FindChild ("lock").GetComponent<SpriteRenderer>();
+		renderer.enabled = false;
+		isLocked = false;
+	}
+
 }
--- Assets/Scripts/gameInitiator.cs
+++ Assets/Scripts/gameInitiator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class gameInitiator : MonoBehaviour {
 
@@ -8,8 +9,17 @@ public class gameInitiator : MonoBehaviour {
 	public int bubblesX = 10;
 	public int bubblesY = 5;
 
+	public int lockedNum = 50;
+	public int poppedNum = 10;
+
 	public Transform normalBubble;
 	public Transform[,] bubbleArray;
+
+	List<int>availableBubbles = new List<int>();
+	List<int>lockedBubbles = new List<int>();
+
+	private int tmp;
+
 
 	void Start () {
 
@@ -22,9 +32,39 @@ public class gameInitiator : MonoBehaviour {
 		for (int i = 0; i < bubblesX; i++) {
 			for (int j = 0; j < bubblesY; j++) {
 				bubbleArray[i,j] = (Transform)Instantiate (normalBubble, new Vector3(startX + 2.0f * i, startY + 2.0f * j, 0), Quaternion.identity);
+				availableBubbles.Add(i + bubblesX * j);
 			}
-			
 		}
+
+		for (int i = 0; i < poppedNum; i++) {
+			tmp = Random.Range (0, availableBubbles.Count);
+			tmp = availableBubbles[tmp];
+			bubbleArray[(tmp % bubblesX), Mathf.FloorToInt(tmp / bubblesX)].BroadcastMessage("startPopped");
+			availableBubbles.Remove(tmp);
+		}
+		
+		for (int i = 0; i < lockedNum; i++) {
+			tmp = Random.Range (0, availableBubbles.Count);
+			tmp = availableBubbles[tmp];
+			bubbleArray[(tmp % bubblesX), Mathf.FloorToInt(tmp / bubblesX)].BroadcastMessage("startLocked");
+			availableBubbles.Remove(tmp);
+			lockedBubbles.Add (tmp);
+		}
+
+		tmp = Random.Range (0, availableBubbles.Count);
+		tmp = availableBubbles[tmp];
+		bubbleArray[(tmp % bubblesX), Mathf.FloorToInt(tmp / bubblesX)].BroadcastMessage("startWithKey");
+
+	}
+
+	void unlockNextBubble(){
+		if (lockedBubbles.Count == 0)
+			return;
+		tmp = lockedBubbles [0];
+		Debug.Log (tmp);
+		lockedBubbles.RemoveAt (0);
+		bubbleArray[(tmp % bubblesX), Mathf.FloorToInt(tmp / bubblesX)].BroadcastMessage("unlockBubble");
+		bubbleArray[(tmp % bubblesX), Mathf.FloorToInt(tmp / bubblesX)].BroadcastMessage("startWithKey");
 	}
 
 }
