120eddd9c91c78460c2cc5ae13084ac7941447a6
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-27 08:21:55 +0300

    Bubbles centered properly

--- Assets/Scripts/gameInitiator.cs
+++ Assets/Scripts/gameInitiator.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class gameInitiator : MonoBehaviour {
 
-	public int bubblesX = 16;
-	public int bubblesY = 16;
+	private int groupNum = 0;
+
+	public int bubblesX = 10;
+	public int bubblesY = 5;
 
 	public Transform normalBubble;
+	public Transform[,] bubbleArray;
 
 	void Start () {
-		float startX = -2.0f * bubblesX / 2.0f;
-		float startY = -2.0f * bubblesY / 2.0f;
+
+		bubbleArray = new Transform[bubblesX,bubblesY];
+
+		float startX = -1.0f * (bubblesX-1);
+		float startY = -1.0f * (bubblesY-1);
+
 
 		for (int i = 0; i < bubblesX; i++) {
 			for (int j = 0; j < bubblesY; j++) {
-				Instantiate (normalBubble, new Vector3(startX + 2.0f * i, startY + 2.0f * j, 0), Quaternion.identity);
+				bubbleArray[i,j] = (Transform)Instantiate (normalBubble, new Vector3(startX + 2.0f * i, startY + 2.0f * j, 0), Quaternion.identity);
 			}
 			
 		}
