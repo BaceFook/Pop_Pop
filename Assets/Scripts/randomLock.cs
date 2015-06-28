38051495e38c396c51aa5147bb9f0cfbf86bb999
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-06-28 16:07:55 +0300

    Random lock sprite

---
+++ Assets/Scripts/randomLock.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomLock : MonoBehaviour {
+	public Sprite[] lockSprites;
+
+	void Start () {
+		GetComponent<SpriteRenderer>().sprite = lockSprites[Random.Range (0, lockSprites.Length)];
+	}
+}
