f017a3a0d601a81a8d29876d33a9aeb4d307a979
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-07-02 16:50:45 +0300

    Client automatically gets ready, finally

--- Assets/Scripts/Network/LobbyManager.cs
+++ Assets/Scripts/Network/LobbyManager.cs
@@ -1,22 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class LobbyManager : NetworkManager {
 
-	// Use this for initialization
+	private NetworkConnection myConnection;
+	
+	public void Setready(){
+//		ClientScene.Ready (client.connection);
+		Debug.Log (NetworkServer.connections.Count);
+		
+		foreach (NetworkConnection conn  in NetworkServer.connections) {
+			if (conn == null)
+				continue;
+			Debug.Log (conn.connectionId);
+			Debug.Log (conn.isReady);
+		}
+	}
+
 	void Start () {
 		PlayerPrefs.SetString("CloudNetworkingId", "124152");
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
+	}
+
+	public override void OnServerReady (NetworkConnection conn)
+	{
+		base.OnServerReady (conn);
+		Debug.Log ("Client pressed ready");
+	}
+
+	public override void OnStartServer ()
+	{
+		base.OnStartServer ();
+		Debug.Log ("I started server");
+	}
+
+	public override void OnStartClient (NetworkClient client)
+	{
+		base.OnStartClient (client);
+		Debug.Log ("I started client");
+		Debug.Log (client.connection.connectionId);
 	}
 	
 	public override void OnServerConnect (NetworkConnection conn)
 	{
 		base.OnServerConnect (conn);
-		Debug.Log ("networkScarter Detects");
+		GameObject.Find ("networkStatus").GetComponent<Text>().text = "Am server + enemy";
+		Debug.Log ("Someone connected to me");
+	}
+
+	public override void OnClientConnect (NetworkConnection conn)
+	{
+		base.OnServerConnect (conn);
+		myConnection = conn;
+		Debug.Log (conn.connectionId);
+		if (NetworkServer.active) {
+			GameObject.Find ("networkStatus").GetComponent<Text>().text = "Am server";
+			Debug.Log ("I connected to myself");
+			ClientScene.Ready (conn);
+		}
+		else
+		{
+			GameObject.Find ("networkStatus").GetComponent<Text>().text = "Am client";
+			Debug.Log ("I connected to someone");
+			ClientScene.Ready (conn);
+		}
+	}
+
+	public override void OnMatchCreate (UnityEngine.Networking.Match.CreateMatchResponse matchInfo)
+	{
+		base.OnMatchCreate (matchInfo);
+		Debug.Log (matchInfo);
+	}
+
+	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+	{
+		base.OnServerAddPlayer (conn, playerControllerId);
+		Debug.Log (playerControllerId);
 	}
 }
--- Assets/Scripts/Network/callbackTester.cs
+++ Assets/Scripts/Network/callbackTester.cs
@@ -3,10 +3,5 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class callbackTester : NetworkBehaviour {
-	
-	public override void OnStartClient ()
-	{
-		base.OnStartClient ();
-		Debug.Log ("callbackTester Detects");
-	}
+
 }
