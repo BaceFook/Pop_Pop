9e2d15a09f4176586145a8847cdde4406e026e7b
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-07-13 00:36:58 +0300

    Pop vs Pop gameplay done

--- Assets/Scripts/Network/PvsPcontroller.cs
+++ Assets/Scripts/Network/PvsPcontroller.cs
@@ -7,7 +7,12 @@ public class PvsPcontroller : NetworkBehaviour {
 	public GameObject wallsPrefab;
 	public GameObject ballPrefab;
 
+	Vector3 lastPoint;
+	LayerMask bubbleMask;
+
 	public void Setup(){
+		bubbleMask = LayerMask.GetMask ("Bubbles");
+
 		GameObject.Find ("WinButton").GetComponent<Button> ().onClick.AddListener(delegate{NetworkPlayer.myPlayer.CmdPvsPpops(1);});
 		GameObject go = (GameObject) GameObject.Instantiate (wallsPrefab, Vector3.zero, Quaternion.identity);
 		go.transform.parent = NetworkController.instance.gameParent.transform;
@@ -32,6 +37,8 @@ public class PvsPcontroller : NetworkBehaviour {
 			NetworkPlayer.myPlayer.bubbles -= number;
 		}
 
+		if (NetworkController.instance.gameEnded)
+			return;
 		GameObject.Find ("BubblesText").GetComponent<Text> ().text = NetworkPlayer.myPlayer.bubbles.ToString ();
 		if (NetworkServer.active)
 			CheckForVictory ();
@@ -43,5 +50,32 @@ public class PvsPcontroller : NetworkBehaviour {
 		else if (NetworkPlayer.enemyPlayer.bubbles >= 100)
 			NetworkController.instance.RpcVictory (NetworkPlayer.myPlayer.netId);
 
+	}
+
+	void Update(){
+		if (NetworkController.instance.gameEnded)
+			return;
+
+		Vector3 point = Vector3.zero;
+		if (Input.GetMouseButton (0)) {
+			point = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		}  else if(Input.touchCount > 0) {
+			point = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+		}
+
+		if (point.magnitude > float.Epsilon) {
+			Debug.Log ((Vector2)point);
+			if (lastPoint.magnitude < float.Epsilon)
+				lastPoint = point;
+			RaycastHit2D[] hits = Physics2D.LinecastAll ((Vector2)point, (Vector2)lastPoint, bubbleMask.value);
+			if (hits.Length > 0)
+				NetworkPlayer.myPlayer.CmdPvsPpops (hits.Length);
+			foreach (RaycastHit2D hit in hits) {
+				hit.transform.BroadcastMessage ("GetPopped");
+			}
+			lastPoint = point;
+		} else {
+			lastPoint = Vector3.zero;
+		}
 	}
 }
